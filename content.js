e12725513463ebf0e093a873c2e39a47aef38c0f
andrejarboe <andrejarboe@gmail.com>
2022-08-25 15:12:19 -0400

    grabbed divs to toggle

---
+++ content.js
@@ -0,0 +1,9 @@
+
+
+// grab divs
+let progressBar = document.getElementsByClassName('ytp-progress-bar');
+let controls = document.getElementsByClassName('ytp-chrome-controls');
+
+
+// toggle hidden
+
